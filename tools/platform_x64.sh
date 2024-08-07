dbf05999a2e8676b2b9350eea854606cc4f60a96
young <shadow.daily.young@gmail.com>
2024-08-07 10:58:20 +0800

    [simple]:update param script [description]: 	update param

--- tools/platform_x64.sh
+++ tools/platform_x64.sh
@@ -1,4 +1,4 @@
 export OS_DEBUG_ARCH=""
 export OS_DEBUG_CROSS_COMPILE=""
 
-echo "qemu-system-x86_64 -kernel linux/arch/x86_64/boot/bzImage -drive file=rootfs.img,format=raw,if=ide -append \"nokaslr console=ttyS0 root=/dev/sda  rootfstype=ext4 rw\" -nographic" > debug.sh
+echo "qemu-system-x86_64 -kernel linux/arch/x86_64/boot/bzImage -drive file=rootfs.img,format=raw,if=ide -append \"nokaslr console=ttyS0 root=/dev/sda  rootfstype=ext4 rw\" -nographic \$@" > debug.sh
