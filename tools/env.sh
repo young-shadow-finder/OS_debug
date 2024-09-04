dc73a3185f0c0c835f878cbfbb5fc9527952a2da
young-shadow-finder <150509938+young-shadow-finder@users.noreply.github.com>
2024-09-04 15:27:38 +0800

    Update env.sh
    update scripts

--- tools/env.sh
+++ tools/env.sh
@@ -1,12 +1,12 @@
-sudo apt-get install git libncurses5-dev build-essential
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-none-linux-gnu-
 sudo apt install make
 sudo apt install flex
 sudo apt install bison
+sudo apt install build-essential
 sudo apt install libncurses-dev
 sudo apt install bzip2
 sudo apt-get install libelf-dev
-
+sudo apt-get install libssl-dev
 
 
